d5e71528c846530bd5ea19713936a94bfb34613f
Regber <regber.888@gmail.com>
2022-12-26 13:25:12 +0300

    Для работы с инжектированной библиотекой добавлен класс Fallout4Conductor

---
+++ InteractiveFallout4/Common/Conductors/Fallou4Conductor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace InteractiveFallout4.Common.Conductors
+{
+    class Fallou4Conductor
+    {
+        // Выделенная именованная память
+        public static MemoryMappedFile SimpleConsoleCommand;
+
+        public static MemoryMappedFile RequestGlobalValue;
+        public static MemoryMappedFile ResponseGlobalValue;
+
+        public Fallou4Conductor()
+        {
+            // Создаст, или подключится к уже созданной памяти с таким именем
+            SimpleConsoleCommand = MemoryMappedFile.CreateOrOpen("Fallout4SimpleConsoleCommand", 512, MemoryMappedFileAccess.ReadWrite);
+
+            RequestGlobalValue = MemoryMappedFile.CreateOrOpen("Fallout4RequestGlobalValue", 512, MemoryMappedFileAccess.ReadWrite);
+            ResponseGlobalValue = MemoryMappedFile.CreateOrOpen("Fallout4ResponseGlobalValue", 512, MemoryMappedFileAccess.ReadWrite);
+        }
+
+        public void SendSimpleConsoleCommand(string consoleCommand)
+        {
+            SimpleConsoleCommand.CreateViewStream().Write(Encoding.Default.GetBytes(consoleCommand + '\0'), 0, consoleCommand.Length + 1);
+        }
+
+        public string GetGlobalValue(string variableName)
+        {
+            RequestGlobalValue.CreateViewStream().Write(Encoding.Default.GetBytes(variableName + '\0'), 0, variableName.Length + 1);
+
+            string responseGlobalValue = "" + '\0';
+
+            //Выполняем до тех пор пока не получим ответ
+            while (responseGlobalValue[0] == '\0')
+            {
+                var stream = ResponseGlobalValue.CreateViewStream();
+
+                byte[] responseGlobalValueByteArray = new byte[stream.Length];
+
+                stream.Read(responseGlobalValueByteArray, 0, responseGlobalValueByteArray.Length);
+
+                responseGlobalValue = Encoding.Default.GetString(responseGlobalValueByteArray).Replace('\n', '\0');
+            }
+
+            //Очищаем поток с ответом
+            ResponseGlobalValue.CreateViewStream().Write(Encoding.Default.GetBytes("" + '\0'), 0, 1);
+
+
+            //если сообщение содержит ошибку
+            if (responseGlobalValue.StartsWith("error"))
+            {
+                return "error";
+            }
+            else
+            {
+                return GetFloatNumber(responseGlobalValue);
+            }
+        }
+
+        private string GetFloatNumber(string message)
+        {
+            return Regex.Match(message, @"\d[\d|.]*[\d|.]").Value;
+        }
+    }
+}
